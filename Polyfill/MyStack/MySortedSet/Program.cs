e9c7899ffe9a7181714b7bda49f4d803e1365e50
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-05-04 21:53:25 +0400

    Create Program.cs

---
+++ Polyfill/MyStack/MySortedSet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySortedSetImp;
+namespace Program;
+
+public class Program
+{
+    public static void Main()
+    {
+        MySortedSet<int> arr = new MySortedSet<int>();
+        arr.Add(5);
+        arr.Add(4);
+        arr.Add(3);
+    }
+}
